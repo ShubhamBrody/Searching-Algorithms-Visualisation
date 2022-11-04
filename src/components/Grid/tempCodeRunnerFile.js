c284825ba209275526f1e13f69ea980f00c6d81b
Shubham Tiwari <st8896464352@gmail.com>
2022-11-04 13:18:59 +0530

    TODO updated

--- src/components/Grid/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-  useEffect((e) => {
-    window.onkeydown((e) => {
-        if(e.key==="Alt") setAltkey(true);
-    });
-    window.onkeyup((e) => {
-        if(e.key==="Alt") setAltkey(false);
-    })
-  })
